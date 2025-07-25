643cdfc1fa8a924507e3d1a069d5cdc9c360c22d
Puru-codes <23f2004566@ds.study.iitm.ac.in>
2025-07-25 13:53:51 +0530

    done with register and login page

--- models.py
+++ models.py
@@ -11,7 +11,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
 
     
--- routes.py
+++ routes.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, request, flash
+from models import db, User, ParkingLot, ParkingSpot, Reservation
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @app.route('/')
 def index():
@@ -9,6 +11,42 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/login', methods=['POST'])
+def login_post():
+    username= request.form.get('username')
+    password= request.form.get('password')
+    user = User.query.filter_by(username=username).first()
+    if not user or not check_password_hash(user.password_hash, password):
+        flash('Invalid username or password.', 'danger')
+        return redirect(url_for('login'))
+    return redirect(url_for('index'))
+
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    confirm_password = request.form.get('confirm_password')
+    name= request.form.get('name')
+
+    user= User.query.filter_by(username=username).first()
+    if user:
+        flash('Username already exists. Please choose a different one.', 'danger')
+        return redirect(url_for('register'))
+    
+    if password != confirm_password:
+        flash('Passwords do not match. Please try again.', 'danger')
+        return redirect(url_for('register'))
+    
+    if not username or not password :
+        flash('All fields are required.', 'danger')
+        return redirect(url_for('register'))
+    
+    password_hash= generate_password_hash(password)
+    new_user = User(username=username, password_hash=password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('login'))
